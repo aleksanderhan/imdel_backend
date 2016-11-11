a9fbbf427461c19ea951307544ad49731d03ab05
Aleksander <aleksanderhan@gmail.com>
2016-11-11 03:17:25 +0100

    expanded ImageModel

--- image_server/test/POST_image_test.py
+++ image_server/test/POST_image_test.py
@@ -1,5 +1,15 @@
-import requests
+import requests, json
 
-r = requests.post('http://127.0.0.1:8000/upload_image/', files={'file': open('test_image.png', 'rb')})
+POST = 	{
+		'text' : 'lorem ipsum',
+		'latitude' : '59.913869',
+		'longitude' : '10.752245'
+		}
+
+r = requests.post(
+	'http://127.0.0.1:8000/upload_image/', 
+	files = {'image' : open('test_image.png', 'rb')}, data=POST)
+
+#r = requests.post('http://127.0.0.1:8000/upload_image/', files = {'image' : open('test_image.png', 'rb')}, text='lorem ipsum', latitude='59.913869', longitude = '10.752245')
 
 print(r.text)
