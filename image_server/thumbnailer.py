08a1f6d15b8fdc4fa7144c4e92e5c3d4ce2665f4
Aleksander Hansen <aleksanderhan@gmail.com>
2016-11-13 03:41:58 +0100

    bug fix

--- image_server/thumbnailer.py
+++ image_server/thumbnailer.py
@@ -2,14 +2,14 @@ from PIL import Image
 import traceback
 import os
 
-save_directory_path = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir, os.pardir)) + '/uploaded_images/thumbs/'
+image_directory_path = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir, os.pardir)) + '/uploaded_images/'
 
 
 def makeThumbnail(filename, size=(160, 90)):
     try:
-        im = Image.open(filename)
+        im = Image.open(image_directory_path + filename)
         im.thumbnail(size)
-        im.save(save_directory_path + filename, "JPEG")
+        im.save(image_directory_path + 'thumbs/' + filename, "JPEG")
     except:
         traceback.print_exc()
 
